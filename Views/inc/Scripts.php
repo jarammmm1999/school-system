8d5ac43628f095ef6a323d7e50942aef06e611cf
Jhon Stewar Moreno Murillo <jhonstiwarmorenomurillo@gmail.com>
2024-03-22 20:12:23 -0500

    version 0.2

--- Views/inc/Scripts.php
+++ Views/inc/Scripts.php
@@ -1,3 +1,4 @@
 <script src="<?=SERVERURL?>node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
 <script src="<?=SERVERURL?>node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?=SERVERURL?>node_modules/sweetalert2/dist/sweetalert2.all.js"></script>
 <script src="<?=SERVERURL?>Views/js/main.js"></script>
--- Views/js/main.js
+++ Views/js/main.js
@@ -2,6 +2,21 @@ var botonValidation = document.getElementById("btn-login");
 botonValidation.textContent = "Acceder";
 
 botonValidation.addEventListener("click", function() {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Signed in successfully"
+      });
     botonValidation.textContent = "Validando Información..";
     setTimeout(function() {
         botonValidation.textContent =  "Acceder";
